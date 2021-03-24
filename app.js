489dcf7b4017f302efcafae648c8a6f343f4f92d
Zack <zacke@sent.com>
2021-03-24 15:06:27 -0400

    adding the completed node.js lab

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const routes = require('./routes/routes');
+const port = 3002;
+const app = express();
+// Require packages and set the port
+
+// Use Node.js body parsing middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true,
+}));
+
+routes(app);
+
+// Start the server
+const server = app.listen(port, (error) => {
+    if (error) return console.log(`Error: ${error}`);
+ 
+    console.log(`Server listening on port ${server.address().port}`);
+});
---
+++ data/config.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql');
+
+// Set database connection credentials
+const config = {
+    host: '162.243.235.211',
+    user: 'user345',
+    password: 'Password1@',
+    database: 'db345',
+};
+
+// Create a MySQL pool
+const pool = mysql.createPool(config);
+
+// Export the pool
+module.exports = pool;
